1f5e7f9a4afc83f1045fc835341d351c43c1ac8f
sancho6040 <qwetito@gmail.com>
2023-05-13 16:22:35 -0500

    player movement

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject arma;
+    public Rigidbody rb;
+    public float speed = 15f;
+    public float turnSpeed = 360f;
+    public Vector3 input;
+
+    private void Start()
+    {
+        DesactivarColliderArma();
+    }
+
+    private void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Look();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //rb.MovePosition(transform.position + (transform.forward * input.normalized.magnitude) * speed * Time.deltaTime);
+        Vector3 playerVelocity = (transform.forward * input.normalized.magnitude) * speed;
+        animator.SetFloat("vel", playerVelocity.magnitude);
+        playerVelocity.y = rb.velocity.y;
+        rb.velocity = playerVelocity;
+    }
+
+    void Look()
+    {
+        if (input != Vector3.zero)
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+
+            var skededInput = matrix.MultiplyPoint3x4(input);
+
+            var relative = (transform.position + skededInput) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    public void ActivarColliderArma()
+    {
+        arma.SetActive(true);
+    }
+
+    public void DesactivarColliderArma()
+    {
+        arma.SetActive(false);
+    }
+}
