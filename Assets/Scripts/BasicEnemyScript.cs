af644ff55d9451488ed551ce2f84cd91daada1be
sancho6040 <qwetito@gmail.com>
2023-05-14 17:39:33 -0500

    Enemy IA
    + animations
    + behaviours
    + navmesh
    
    working on damage

---
+++ Assets/Scripts/BasicEnemyScript.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class BasicEnemyScript : MonoBehaviour
+{
+    public GameObject DamageArea;
+    private NavMeshAgent agent;
+    private Transform player;
+    private Animator animator;
+
+    public LayerMask playerLayer;
+
+    public float health = 120;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked = false;
+
+    //states
+    public float attackRange;
+    public bool bIsPlayerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        bIsPlayerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if (bIsPlayerInAttackRange)
+        {
+            AttackPlayer();
+        }
+        else
+        {
+            ChasePlayer();
+        }
+
+        animator.SetFloat("vel", agent.velocity.magnitude);
+    }
+
+    private void ChasePlayer()
+    {
+        Debug.Log("chasing");
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        Debug.Log("attacking");
+        //the sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        Vector3 playerLookAt = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.LookAt(playerLookAt);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code 
+            animator.SetTrigger("attack");
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int inDamage)
+    {
+        health -= inDamage;
+
+        if (health <= 0)
+        {
+            // Instantiate(EnemyPrefab, GetRandonWaypoint(), Quaternion.identity);
+            Invoke(nameof(DestroyEnemy), 0.5f);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "arma")
+        {
+            TakeDamage(15);
+        }
+    }
+
+    public void ActivateDamageArea()
+    {
+        DamageArea.SetActive(true);
+    }
+
+    public void DeactivateDamageArea()
+    {
+        DamageArea.SetActive(false);
+    }
+}
